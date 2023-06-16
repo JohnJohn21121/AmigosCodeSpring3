d3f4627b19834075219f64cc19028b86d0543706
JohnJohn21121 <john.abreu21121@gmail.com>
2023-06-16 12:18:02 -0300

    finished the first section and setting of the course

--- src/main/java/com/JohnJohn21121/Main.java
+++ src/main/java/com/JohnJohn21121/Main.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class Main {
@@ -14,7 +16,16 @@ public class Main {
     }
 
     @GetMapping("/greeting")
-    public String greet(){
-        return "Hello Human";
+    public GreetResponse greet() {
+        GreetResponse response = new GreetResponse("Hello you, Human",
+                List.of("Spanish", "English", "French"),
+                new Person("John", 24));
+        return response;
+    }
+
+    record Person(String name, int age) {
+    }
+
+    record GreetResponse(String greet, List<String> favLanguage, Person person) {
     }
 }
